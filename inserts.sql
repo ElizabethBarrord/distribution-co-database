972fbae75403d319c9831b01d7ff7e9913cfcb29
Lynette Lopez <lopezl@miamioh.edu>
2018-04-25 22:43:46 -0400

    Added data for query 4

--- inserts.sql
+++ inserts.sql
@@ -1,21 +1,21 @@
 INSERT INTO warehouse
 	(wcode, wname)
 VALUES
-	('warehouse0001','Warehouse 1'),
-	('warehouse0002','Warehouse 2'),
-	('warehouse0003','Warehouse 3'),
-	('warehouse0004','Warehouse 4'),
-	('warehouse0005','Warehouse 5'),
-	('warehouse0006','Warehouse 6');
+	('warehouse0001', 'Warehouse 1'),
+	('warehouse0002', 'Warehouse 2'),
+	('warehouse0003', 'Warehouse 3'),
+	('warehouse0004', 'Warehouse 4'),
+	('warehouse0005', 'Warehouse 5'),
+	('warehouse0006', 'Warehouse 6');
 
 INSERT INTO abin
 	(binno, wcode, capacity)
 VALUES
-	(0001,'warehouse0001', 50),
-	(0004,'warehouse0004', 40),
-	(0001,'warehouse0005', 40),
-	(0003,'warehouse0002', 50),
-	(0005,'warehouse0006', 50);
+	(0001, 'warehouse0001', 50),
+	(0004, 'warehouse0004', 40),
+	(0001, 'warehouse0005', 40),
+	(0003, 'warehouse0002', 50),
+	(0005,' warehouse0006', 50);
 
 INSERT INTO manager
   	(empno, mname)
@@ -29,39 +29,40 @@ INSERT INTO manager
 INSERT INTO part
 	(partno, pname, bdate, bqty, empno)
   VALUES
-	(1114, 'tire', '2018-23-03', 12, 2001),
+	(1114, 'tire', '2018-23-03', 12, 2002),
 	(1002, 'mirror', '2018-20-01', 15, 2004),
 	(1003, 'side door', '2018-20-01', 35, 2005),
-	(1004, 'wheel', '2018-10-10', 40, 2001),
-	(1005, 'gas pedal', '2018-05-02', 47, 2001);
+	(1004, 'wheel', '2018-10-10', 40, 2003),
+	(1005, 'gas pedal', '2018-05-02', 47, 2001),
+    (1006, 'brake', '2017-29-12', 47, 2001);
 
 INSERT INTO batch
   	(batchno, datein, size, binno, wcode)
   VALUES
-  	(3001, '2018-01-01', 100, 0001,'warehouse0001'),
-  	(3002, '2018-03-30', 210, 0004,'warehouse0004'),
-  	(3003, '2018-05-11', 300, 0001,'warehouse0005'),
-  	(3004, '2018-15-06', 50, 0003,'warehouse0002'),
-  	(3005, '2018-01-08', 70, 0005,'warehouse0006');
+  	(3001, '2018-01-01', 100, 0001, 'warehouse0001'),
+  	(3002, '2018-03-30', 210, 0004, 'warehouse0004'),
+  	(3003, '2018-05-11', 300, 0001, 'warehouse0005'),
+  	(3004, '2018-15-06', 50, 0003, 'warehouse0002'),
+  	(3005, '2018-01-08', 70, 0005, 'warehouse0006');
 
 INSERT INTO item
   	(itemno, batchno, partno, idateout, empno)
   VALUES
-  	(4001, 3002, 1114,'2018-01-01', 2005),
-  	(4002, 3003, 1003,'2018-05-11', 2003),
-  	(4003, 3001, 1004,'2018-04-12', 2003),
-  	(4004, 3005, 1004,'2018-04-09', 2002),
-  	(4005, 3002, 1005,'2018-04-29', 2003),
-		(4006, 3002, 1005,'2018-04-30', 2002);
+  	(4001, 3002, 1114, '2018-01-01', 2005),
+  	(4002, 3003, 1003, '2018-05-11', 2003),
+  	(4003, 3001, 1004, '2018-04-12', 2003),
+  	(4004, 3005, 1004, '2018-04-09', 2002),
+  	(4005, 3002, 1005, '2018-04-29', 2003),
+	(4006, 3002, 1006, '2018-04-30', 2002);
 
 INSERT INTO assembly
     	(assemblyno, adateout, empno)
-    VALUES
-    	(5001, '2018-07-13', 2004),
-    	(5002, '2018-05-03', 2001),
-    	(5003, '2018-10-10', 2004),
-    	(5004, '2018-07-13', 2003),
-    	(5005, '2018-07-13', 2005);
+VALUES
+	(5001, '2018-07-13', 2004),
+	(5002, '2018-05-03', 2001),
+	(5003, '2018-10-10', 2004),
+	(5004, '2018-07-13', 2003),
+	(5005, '2018-07-13', 2005);
 
 INSERT INTO comprises
 	(itemno, partno, batchno, assemblyno)
@@ -71,7 +72,7 @@ VALUES
 	(4003, 1004, 3001, 5003),
 	(4004, 1004, 3005, 5004),
 	(4005, 1005, 3002, 5005),
-	(4006, 1005, 3002, 5005);
+	(4006, 1006, 3002, 5005);
 
 INSERT INTO instances
 	(itemno, partno, batchno)
